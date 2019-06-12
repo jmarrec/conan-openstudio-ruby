b132f039eafa6559bf268dd4a32f51bfbd3a5335
Julien Marrec <julien.marrec@gmail.com>
2019-06-12 10:02:35 +0200

    Improve reporting. Got thrown off looking at AppVeyor build history that it was finding 89 libs before and after PR

--- conanfile.py
+++ conanfile.py
@@ -132,16 +132,25 @@ class OpenstudiorubyConan(ConanFile):
             self.output.error("Globbing: {}".format(glob_pattern))
             raise ConanException("Didn't find the libraries!")
 
-        self.output.success("Found {} libs".format(len(libs)))
+        # Remove the non-static VS libs
+        if self.settings.os == "Windows":
+            non_stat_re = re.compile(r'x64-vcruntime[0-9]+-ruby[0-9]+\.lib')
+            exclude_libs = [x for x in libs
+                            if non_stat_re.search(x)]
+            print("Excluding {} non-static libs: {}".format(len(exclude_libs),
+                                                            exclude_libs))
+
+            # Now we actually exclude it
+            libs = list(set(libs) - set(exclude_libs))
 
         # Relative to package folder: no need unless explicitly setting glob
         # to package_folder above
         # libs = [os.path.relpath(p, start=self.package_folder) for p in libs]
 
-        # Keep only the names, remove the non-static VS libs
-        non_stat_re = re.compile(r'x64-vcruntime[0-9]+-ruby[0-9]+\.lib')
-        self.cpp_info.libs = [os.path.basename(x) for x in libs
-                              if not non_stat_re.search(x)]
+        # Keep only the names:
+        self.cpp_info.libs = [os.path.basename(x) for x in libs]
+
+        self.output.success("Found {} libs".format(len(libs)))
 
         # self.cpp_info.libdirs = ['lib', 'lib/ext', 'lib/enc']
         # Equivalent automatic detection
